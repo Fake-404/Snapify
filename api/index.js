57bc06deb901ed3d02a0289832df7999ffb7d0e6
Fake-404 <120786742+Fake-404@users.noreply.github.com>
2025-01-02 14:35:28 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,9 +7,11 @@ app.use(express.json());
 // Routes
 import test from '../routers/test.js';
 import InstagramPost from '../routers/instagramPost.js';
+import Test from '../routers/test.js';
 
 app.use('/', test);
 app.use('/igpost', InstagramPost);
+app.use('/test', Test);
 
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
